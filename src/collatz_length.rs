4d773ffcf318ddf032fc5972567620adfafc2ba2
RubenPari <rubenpari.developer@gmail.com>
2025-01-17 18:28:59 +0100

    finished exercise 2

--- src/collatz_length.rs
+++ src/collatz_length.rs
@@ -1,1 +1,27 @@
-pub(crate) fn collatz_length() -> u32 {}
+///
+///
+/// # Arguments
+///
+/// * `n`:
+///
+/// returns: u32
+///
+pub(crate) fn collatz_length(mut n: i32) -> u32 {
+    let mut length: u32 = 1;
+
+    while n != 1 {
+        if n % 2 == 0 {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        length += 1;
+    }
+
+    length
+}
+
+#[test]
+fn test_collatz_length() {
+    assert_eq!(collatz_length(11), 15);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-mod fibonacci;
 mod collatz_length;
+mod fibonacci;
 
-use fibonacci::fibonacci;
 use collatz_length::collatz_length;
+use fibonacci::fibonacci;
 
 fn main() {
     // Exercise 1
     println!("{}", fibonacci(10));
-    
+
     // Exercise 2
-    
+    println!("{}", collatz_length(11));
 }
